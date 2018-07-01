1629f9648ff65ae5317358b68990d5f02bc9c8fd
Joao Santos <jrmsantos15@gmail.com>
2018-07-01 13:58:12 +0100

    wohoo

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Popup from "./ui/Popup.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './ui/Popup.js';
 
-const injectPoint = document.querySelector(".TweetBoxToolbar");
+const injectPoint = document.querySelector('.TweetBoxToolbar')
 
-const web3Script = chrome.extension.getURL("scripts/web3Access.js");
-const node = document.getElementsByTagName("body")[0];
-const script = document.createElement("script");
-script.setAttribute("type", "text/javascript");
-script.setAttribute("src", web3Script);
-node.appendChild(script);
+const web3Script = chrome.extension.getURL('scripts/web3Access.js')
+const node = document.getElementsByTagName('body')[0]
+const script = document.createElement('script')
+script.setAttribute('type', 'text/javascript')
+script.setAttribute('src', web3Script)
+node.appendChild(script)
--- app/scripts/web3Access.js
+++ app/scripts/web3Access.js
@@ -4,30 +4,29 @@ import Popup from './ui/Popup.js'
 import jsConnector from 'tweets-connector'
 
 window.addEventListener('load', function () {
-  window.setTimeout(() => {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-    // Use the browser's ethereum provider
-      const provider = web3.currentProvider
-      console.log('Using metamask!')
-    } else {
-      console.log('No web3? You should consider trying MetaMask!')
-    }
 
-    const injectPoint = document.querySelector('.TweetBoxToolbar')
+  if (typeof web3 !== 'undefined') {
+    // Use the browser's ethereum provider
+    console.log('Using metamask!')
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+  }
+    // var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+
+  const injectPoint = document.querySelector('.TweetBoxToolbar')
 
   // Inject button
-    const popupInjectPoint = injectPoint.appendChild(
+  const popupInjectPoint = injectPoint.appendChild(
     document.createElement('div')
   )
 
-    ReactDOM.render(<Popup />, popupInjectPoint)
+  ReactDOM.render(<Popup />, popupInjectPoint)
 
 /*
   allTweets.map(tweet => {
     console.log('Tweet hashtag: ', tweet.getElementsByTagName('a')[0])
   }) */
 
-    jsConnector.init('0xc301ef8082e0f65d5edd979351218f1f8a851b1e ', false, window.web3)
-  }, 10000)
+  jsConnector.init('0xc301ef8082e0f65d5edd979351218f1f8a851b1e', false, window.web3)
 })
